536beba71ed21c1fe72a5cfcb28e2cb9b558dbb8
phumzani101 <phumzani101@gmail.com>
2020-01-16 18:07:45 +0200

    Add GET /todos route

--- server/routes/todo.routes.js
+++ server/routes/todo.routes.js
@@ -3,6 +3,14 @@ const Todo = require('../model/todo.model')
 
 const Router = express.Router()
 
+Router.get('/', (req, res) => {
+    Todo.find().then((todos) => {
+        res.json({todos})
+    }, (e) => {
+        res.status(400).json({error: e.message})
+    })
+})
+
 Router.post('/', (req, res) => {
     const todo = new Todo({
         text: req.body.text
